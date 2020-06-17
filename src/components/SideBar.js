f9dc028e41a1a47bfbbf4686e7c2e1be34f01d2e
lamnehihi <51854050+lamnehihi@users.noreply.github.com>
2020-06-17 20:00:31 +0700

    Update SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./SideBar.css";
 
-import index from "../img/work.svg";
-import blogs from "../img/mail.svg";
-import about from "../img/user.svg";
+import index from "https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fwork.svg?v=1592353215820";
+import blogs from "https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fmail.svg?v=1592353342807";
+import about from "https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fuser.svg?v=1592353381855";
 
 
 class SideBar extends Component {
