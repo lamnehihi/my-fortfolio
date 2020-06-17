f664c4c78ebdc8256c511ff4dd0031cac84eec58
lamnehihi <51854050+lamnehihi@users.noreply.github.com>
2020-06-17 20:02:14 +0700

    Update SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import "./SideBar.css";
 
-import index from "https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fwork.svg?v=1592353215820";
-import blogs from "https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fmail.svg?v=1592353342807";
-import about from "https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fuser.svg?v=1592353381855";
 
 
 class SideBar extends Component {
@@ -16,17 +13,17 @@ class SideBar extends Component {
           <ul>
             <li>
               <Link to="/">
-                <img src={index}/>
+                <img src="https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fwork.svg?v=1592353215820" />
               </Link>
             </li>
             <li>
               <Link to="/blog">
-                <img src={blogs}/>
+                <img src="https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fmail.svg?v=1592353342807" />
               </Link>
             </li>
             <li>
               <Link to="/about">
-                <img src={about}/>
+                <img src="https://cdn.glitch.com/1e4bb2b9-2471-425f-8d20-b8cc90ece1e5%2Fuser.svg?v=1592353381855" />
               </Link>
             </li>
           </ul>
